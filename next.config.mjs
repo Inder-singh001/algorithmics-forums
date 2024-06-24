d3b3156b18984576b4d25e010ff5ab84038ea6b7
Inderpreet_Singh <singhinderpreet238@gmail.com>
2024-06-24 17:32:59 +0530

    mui-sass_setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import path from "path";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(process.cwd(), "./public/scss")],
+    },
+};
 export default nextConfig;
