180e1a1a95c64b67444bb64be13d84abeabbdb21
PRATHAM YADAV <prayad0620@gmail.com>
2024-06-29 17:02:01 +0530

    dashboard-rightcard

---
+++ src/app/components/rightcard.js
@@ -0,0 +1,157 @@
+import { Grid, Container, Card, CardContent ,Typography} from "@mui/material";
+import Close from '@mui/icons-material/Close';
+import '../../../public/sass/pages/card.scss';
+import { KeyboardArrowRight, Pages } from '@mui/icons-material';
+
+
+const RightCard = () => {
+    return (
+            <Container>
+                <Grid container>
+                    <Grid item>
+                        <div className="inner_parent">
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Lorem ipsum dollor sit amet
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>Your answers and posts
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Spaces you're earning from
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>iOS Devs
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Lorem ipsum dollor sit amet
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>Your answers and posts
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Lorem ipsum dollor sit amet
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>iOS Devs
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Lorem ipsum dollor sit amet
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>Your answers and posts
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=' carder'>
+                                <div className='head'>
+                                    <div className='text'>
+                                        Lorem ipsum dollor sit amet
+                                    </div>
+                                    <div className='icon'>
+                                        <Close/>
+                                    </div>
+                                </div>
+                                <div className='description'>
+                                    <div className='des_left'>
+                                        <div className='des_top'>
+                                         <Pages/>iOS Devs
+                                        </div>
+                                        <div className='des_bottom'>
+                                        Lorem ipsum dolor sit amet consectetur. Turpis tortor 
+                                        </div>
+                                    </div>
+                                    <div className='des_right'>
+                                        <KeyboardArrowRight/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+            </Container>
+    )
+}
+export default RightCard;
