7d4237f8f62007705a0310c37aef9e8924145941
PRATHAM YADAV <prayad0620@gmail.com>
2024-07-16 15:22:24 +0530

    profile-edit-done

--- src/app/components/profileEdit.js
+++ src/app/components/profileEdit.js
@@ -13,14 +13,16 @@ import { validatorMake, foreach, postApi, getApi } from '../../helpers/General';
 import { toast } from 'react-toastify';
 import { useEffect } from "react";
 import { tokenName, getToken } from "@/dataCenter/LocalStorage";
+import { HelperText } from "./helpertext";
 
 const ProfileEdit = () => {
 
   const getData = async (token) => {
     try {
       let res = await getApi('/user/profile')
-      let {data} = res.data;
-      return data;
+      console.log(res)
+      return (res.data);
+
     }
     catch (error) {
       console.error("Error fetching user data:", error);
@@ -38,36 +40,40 @@ const ProfileEdit = () => {
 
   // let [formData, setFormData] = useState(defaultValue)
   // let [errors, setErrors] = useState(defaultValue)
+
   const [formData, setFormData] = useState({
     first_name: '',
     last_name: '',
     about_me: '',
     email: '',
+    password: '',
   });
-  
+
   const [errors, setErrors] = useState({
     first_name: '',
     last_name: '',
     about_me: '',
     email: '',
+    password: '',
   });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const token = getToken(tokenName.LOGIN_TOKEN);
-        const userData = await getData(token); 
+        const userData = await getData(token.data);
         if (userData) {
           setFormData({
             first_name: userData.first_name || '',
             last_name: userData.last_name || '',
             about_me: userData.about_me || '',
             email: userData.email || '',
+            password: userData.password || '',
           });
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
-        
+
       }
     };
 
@@ -107,17 +113,15 @@ const ProfileEdit = () => {
   let handleSubmit = async (e) => {
     e.preventDefault()
     let validationRules = await validatorMake(formData, {
-      "first_name": "",
-      "last_name": "",
-      "about_me": "",
-      "email": "|email",
-      // // "password":"required",
+      "first_name": "required",
+      "last_name": "required",
+      "about_me": "required",
+      "email": "required|email",
+      "password":"required",
 
     })
-    let token = getToken(tokenName.LOGIN_TOKEN)
-    console.log(token)
-    let userData = await getData(token)
-    console.log(userData)
+    let coin = getToken(tokenName.LOGIN_TOKEN)
+    let userData = await getData(coin.data)
     if (!validationRules.fails()) {
       console.log(formData, "formData")
       let resp = await postApi(`/user/update/${userData._id}`, formData)
@@ -125,7 +129,13 @@ const ProfileEdit = () => {
       if (resp.status) {
         // otp screen
         toast.success(resp.message)
-        setFormData(defaultValue);
+        setFormData({
+          first_name: userData.first_name || '',
+          last_name: userData.last_name || '',
+          about_me: userData.about_me || '',
+          email: userData.email || '',
+          password: userData.password || '',
+        });
         router.push('/profile')
       }
       else {
@@ -136,7 +146,7 @@ const ProfileEdit = () => {
           toast.error(resp.message)
         }
       }
-       console.log(resp, "resp")
+      console.log(resp, "resp")
     }
     else {
       handleErrors(validationRules.errors.errors)
@@ -144,17 +154,11 @@ const ProfileEdit = () => {
     }
   }
   const router = useRouter();
-  // const [showPassword, setShowPassword] = useState(false);
-
-  // const handleSaveChanges = () => {
-
-  // }
-
-  // const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  // const handleMouseDownPassword = (event) => {
-  //   event.preventDefault();
-  // }
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  }
 
   return (
 
@@ -177,50 +181,59 @@ const ProfileEdit = () => {
           <div className="text_section">
             <InputLabel htmlFor="first-name">First Name</InputLabel>
             <Input
-              id="standard-basic"
+              id="First_name"
               placeholder="First Name"
               name="first_name"
               value={formData.first_name}
               onChange={handleInputChange}
-            // helperText={errors.first_name ? errors.first_name : ''}
+              // error={!!errors.first_name}
+            // helperText={errors.first_name || ''}
             />
+            <HelperText error={errors.first_name ? errors.first_name : ""} />
           </div>
           <div className="text_section">
             <InputLabel htmlFor="lat_name">Last Name</InputLabel>
             <Input
-              id="standard-basic"
+              id="last_name"
               placeholder="Last Name"
               name="last_name"
               value={formData.last_name}
               onChange={handleInputChange}
-            // helperText={errors.last_name ? errors.last_name : ''}
+              // error={!!errors.last_name}
+              // helperText={errors.last_name || ''}
             />
+             <HelperText error={errors.last_name ? errors.last_name : ""} />
           </div>
           <div className="text_section">
-            <InputLabel htmlFor="description">About</InputLabel>
+            <InputLabel htmlFor="about_me">About</InputLabel>
             <Input
               id="standard-multiline-flexible"
               placeholder="Write something about yourself!"
               name="about_me"
               value={formData.about_me}
               onChange={handleInputChange}
-              // helperText={errors.about_me ? errors.about_me : ''}
+              // error={!!errors.about_me}
+              // // helperText={errors.about_me || ''}
+              // helperText={errors.about_me ? errors.about_me : ""}
               // rows={6}
               multiline
             />
+            <HelperText error={errors.about_me ? errors.about_me : ""} />
           </div>
           <div className="text_section">
             <InputLabel htmlFor="email">Email</InputLabel>
             <Input
-              id="standard-basic"
+              id="email"
               placeholder="Email"
               name="email"
               value={formData.email}
               onChange={handleInputChange}
-            // helperText={errors.email ? errors.email : ''}
+              // error={!!errors.email}
+              // helperText={errors.email || ''}
             />
+             <HelperText error={errors.email ? errors.email: ""} />
           </div>
-          {/* <div className="text_section">
+          <div className="text_section">
             <div className="label_section">
               <div className="pass_area">
                 <InputLabel htmlFor="standard-adornment-password">
@@ -254,7 +267,8 @@ const ProfileEdit = () => {
               //   'aria-label': 'weight',
               // }}
             />
-          </div> */}
+             <HelperText error={errors.password ? errors.password: ""} />
+          </div>
 
         </div>
       </form>
